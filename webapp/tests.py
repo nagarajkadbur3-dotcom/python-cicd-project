5aca098d0dfddf533899e0017716eb09fbf12304
Nagaraj <nagarajkadabur4@gmail.com>
2026-08-25 10:46:38 +0530

    Add Django application test

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_home_page(self):
+        response = self.client.get(reverse("home"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Python CI/CD Project")
